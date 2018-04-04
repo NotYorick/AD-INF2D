8b6153a7a64ceaa1ac0629518f70c2e54221dfdd
Daanniello <daansmits_@hotmail.com>
2018-04-04 22:51:18 +0200

    Almost done

--- src/NewLinkedListTest.java
+++ src/NewLinkedListTest.java
@@ -1,10 +1,59 @@
-import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class NewLinkedListTest {
 
-    @Before
-    public void setUp() throws Exception {
+    @Test
+    public void add() {
+        NewLinkedList test = new NewLinkedList();
+        test.add("test");
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        assertEquals(test.get(3),"test3");
+        assertEquals(test.get(2),"test2");
+        assertEquals(test.get(1),"test1");
+    }
+
+    @Test
+    public void contains() {
+        NewLinkedList test = new NewLinkedList();
+        test.add("test");
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        assertEquals(test.contains("test5"),true);
+        assertEquals(test.contains("test7"),false);
+    }
+
+    @Test
+    public void indexOf() {
+        NewLinkedList test = new NewLinkedList();
+        test.add("test");
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        assertEquals(test.indexOf("test4"),4);
+        assertEquals(test.indexOf("test5"),5);
+    }
+
+    @Test
+    public void remove() {
+        NewLinkedList test = new NewLinkedList();
+        test.add("test");
+        test.add("test1");
+        test.add("test2");
+        test.add("test3");
+        test.add("test4");
+        test.add("test5");
+        test.remove(3);
+        assertEquals(test.contains("test3"),false);
     }
 }
