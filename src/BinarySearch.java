f6195ab0c2792e0f3d2f60787030b5d101090a63
NotYorick <yorickguichelaar@gmail.com>
2018-04-04 19:32:39 +0200

    BinarySearch and Queue v1
    BinarySearch on ints and Strings with compareTo()
    Queue with it's own functions

---
+++ src/BinarySearch.java
@@ -0,0 +1,57 @@
+public class BinarySearch {
+
+    public int binarySearch(int[] array, int key, int start, int end)
+    {
+        // if the key isn't in the array, the recursion will call this method with start greater than end
+        if (start > end)
+        {
+            return -1;
+        }
+
+        // take the middle of the array and compare it to the key
+        int middle = (start + end) / 2;
+        if (array[middle] == key)
+        {
+            return middle;
+        }
+        else if (key < array[middle])
+        {
+            // binarySearch on the left half
+            return binarySearch(array, key, start, middle - 1);
+        }
+        else
+        {
+            // binarySearch on the right half
+            return binarySearch(array, key, middle + 1, end);
+        }
+    }
+
+    public int binarySearchString(String[] array, String key, int start, int end)
+    {
+        // if the key isn't in the array, the recursion will call this method with start greater than end
+        if (start > end)
+        {
+            return -1;
+        }
+
+        // take the middel of the array
+        int middle = (start + end) / 2;
+        // compare the string value of the middle to the key
+        int cmp = array[middle].compareTo(key);
+        if (cmp == 0)
+        {
+            // return the index of the key
+            return middle;
+        }
+        else if (cmp > 0)
+        {
+            // binarySearchString on the left half
+            return binarySearchString(array, key, start, middle -1);
+        }
+        else
+        {
+            // binarySearchString on the right half
+            return binarySearchString(array, key, middle + 2, end);
+        }
+    }
+}
--- src/Queue.java
+++ src/Queue.java
@@ -6,37 +6,28 @@ public class Queue<E> extends LinkedList<E>
 {
     private LinkedList<E> list = new LinkedList<E>();
 
-    public boolean add(E e)
-    {
-
-        return false;
-    }
-
     public boolean offer(E e)
     {
-        //
-        list.addLast(e);
-        return true;
+        // add function from Collection returns true if the Collection changed
+        return list.add(e);
     }
 
     public E peek(E e)
     {
+        // return the first element of the list
         return list.getFirst();
     }
 
     public E poll()
     {
+        // check if the list is empty
         if (list.size() == 0)
         {
             return null;
         }
+        // store the element that will be removed to return it later
         E oldfirst = list.getFirst();
         list.remove(oldfirst);
         return oldfirst;
-    }
-
-    public E remove()
-    {
-        return null;
     }
 }
