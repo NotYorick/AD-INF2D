04fcc2c1adbbbea18045bc5cccb723ba1bc88efa
GertJanB10 <gertjanboertien@gmail.com>
2018-04-05 02:45:33 +0200

    test classes

---
+++ src/BinarySearchTreeTest.java
@@ -0,0 +1,81 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gert Jan
+ * 
+ * Test class for BinarySearchTree
+ */
+public class BinarySearchTreeTest
+{
+    private int[] testSortedArray;
+    private BinarySearchTree BTS;
+    
+    public BinarySearchTreeTest()
+    {
+        //Set up an array filled with random unsorted values
+        testSortedArray = new int[]{1,2,4,4,6,10,12,22,25,43,45,66,67,90,99,100,201,320,345,789 };
+
+        //Create an instance of the InsertionSort class
+        BTS = new BinarySearchTree(testSortedArray);
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    /**
+     * Test of sortedArrayToBST, search and getRoot method, of class BinarySearchTree.
+     */
+    @Test
+    public void testSortedArrayBSTFunctions()
+    {
+        System.out.println("sortedArrayToBST, Search, getRoot");
+        int result = BTS.search(BTS.getRoot(),6).key;
+        assertEquals(result, 6);
+    }
+
+    /**
+     * Test of search method for invalid values (valid has already been tested), of class BinarySearchTree.
+     */
+    @Test(expected=NullPointerException.class)
+    public void testSearch()
+    {
+        int result = BTS.search(BTS.getRoot(),7).key;
+    }
+
+    /**
+     * Test of insert method, of class BinarySearchTree.
+     */
+    @Test
+    public void testInsert()
+    {
+        System.out.println("insert");
+        int key = 7;
+        BTS.insert(key);
+        int result = BTS.search(BTS.getRoot(),7).key;
+        assertEquals(result,7);
+    }
+
+    /**
+     * Test of deleteNode and MinValue methods, of class BinarySearchTree.
+     */
+    @Test(expected=NullPointerException.class)
+    public void testDeleteNodeMinValue()
+    {
+        System.out.println("deleteNode");
+        int key = 6;
+        BTS.deleteNode(key);
+        int result = BTS.search(BTS.getRoot(),7).key;
+    }
+
+}
---
+++ src/InsertionSortTest.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Gert Jan
+ * 
+ * Test class for InsertionSort
+ */
+public class InsertionSortTest
+{
+    private int[] testArray;
+    private InsertionSort sorter;
+    
+    public InsertionSortTest()
+    {
+        //Set up an array filled with random unsorted values
+        testArray = new int[]{4,25,45,12,66,320,10,99,4,201,345,22,43,2,6,67,789,90,100,1};
+
+        //Create an instance of the InsertionSort class
+        sorter = new InsertionSort(testArray);
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+
+    /**
+     * Test wheter the array is sorted properly.
+     */
+    @Test
+    public void testGetSortedArray()
+    {
+        System.out.println("getSortedArray");
+        int[] expResult = new int[] {1,2,4,4,6,10,12,22,25,43,45,66,67,90,99,100,201,320,345,789 };
+        int[] result = sorter.getSortedArray();
+        assertArrayEquals(expResult, result);
+    }
+    
+}
