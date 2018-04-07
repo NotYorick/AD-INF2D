75a6542fbfdecd343faa742a99d0926b87c201a6
Daanniello <daansmits_@hotmail.com>
2018-04-07 13:30:12 +0200

    Linked list test met dataset

--- src/NewLinkedListTest.java
+++ src/NewLinkedListTest.java
@@ -1,59 +1,62 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class NewLinkedListTest {
 
+    protected NewLinkedList collection;
+    CSVReader lego;
+    ArrayList<String[]> array;
+
+    @Before
+    public void setUp() throws Exception {
+        lego = new CSVReader();
+        array = lego.getLegoArrayList();
+        collection = new NewLinkedList();
+
+        for(String[] number : array){
+            String numb = number[0];
+            numb = numb.replace("\"", "");
+            collection.add(Integer.parseInt(numb));
+        }
+    }
+
     @Test
     public void add() {
-        NewLinkedList test = new NewLinkedList();
-        test.add("test");
-        test.add("test1");
-        test.add("test2");
-        test.add("test3");
-        test.add("test4");
-        test.add("test5");
-        assertEquals(test.get(3),"test3");
-        assertEquals(test.get(2),"test2");
-        assertEquals(test.get(1),"test1");
+        assertEquals(collection.get(0),10246);
+        assertEquals(collection.get(1),10247);
+        assertEquals(collection.get(2),10248);
+        assertEquals(collection.get(18),10596);
     }
 
     @Test
     public void contains() {
-        NewLinkedList test = new NewLinkedList();
-        test.add("test");
-        test.add("test1");
-        test.add("test2");
-        test.add("test3");
-        test.add("test4");
-        test.add("test5");
-        assertEquals(test.contains("test5"),true);
-        assertEquals(test.contains("test7"),false);
+        assertEquals(collection.contains(3170),true);
+        assertEquals(collection.contains(10246),true);
+        assertEquals(collection.contains(99999999),false);
     }
 
     @Test
     public void indexOf() {
-        NewLinkedList test = new NewLinkedList();
-        test.add("test");
-        test.add("test1");
-        test.add("test2");
-        test.add("test3");
-        test.add("test4");
-        test.add("test5");
-        assertEquals(test.indexOf("test4"),4);
-        assertEquals(test.indexOf("test5"),5);
+        int count = 0;
+        for(Object o : collection){
+
+            assertEquals(collection.indexOf(o),count);
+            count++;
+            if(count > 100){
+                break;
+            }
+        }
     }
 
     @Test
     public void remove() {
-        NewLinkedList test = new NewLinkedList();
-        test.add("test");
-        test.add("test1");
-        test.add("test2");
-        test.add("test3");
-        test.add("test4");
-        test.add("test5");
-        test.remove(3);
-        assertEquals(test.contains("test3"),false);
+        collection.remove(0);
+        assertEquals(collection.contains(10246),false);
+
     }
 }
